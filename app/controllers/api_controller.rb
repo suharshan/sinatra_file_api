7815adfc5a4058b74f3c4f0b210e2e2bb78dd8f3
Pubudu Perera <suharshan22@gmail.com>
2019-01-31 20:13:46 +0800

    Fixed indentation issue

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,46 +1,46 @@
 class APIController < Sinatra::Base
 
-	# Disabling static file direct serving
+  # Disabling static file direct serving
   configure do
-		set :public_folder, 'static'
-		disable :static
-	end
+    set :public_folder, 'static'
+    disable :static
+  end
 
-	# Display help
-	['/', '/help'].each do |route|
- 		get route do
-			output =<<-EOS.gsub(/^[ \t]+(?=\S)/, '')
-				Upload file
-				-----------
-				curl -X POST -F \"file=@sample.png\" http://<server ip>:8080/files
+  # Display help
+  ['/', '/help'].each do |route|
+    get route do
+      output =<<-EOS.gsub(/^[ \t]+(?=\S)/, '')
+        Upload file
+        -----------
+        curl -X POST -F \"file=@sample.png\" http://<server ip>:8080/files
 
-				Retirieve file
-				--------------
-				curl http://<server ip>:8080/files/sample.png
+        Retirieve file
+        --------------
+        curl http://<server ip>:8080/files/sample.png
 
-				Delete file
-				-----------
-				curl -X DELETE http://<server ip>:8080/files/sample.png
-			EOS
-			output
-		end
-	end
+        Delete file
+        -----------
+        curl -X DELETE http://<server ip>:8080/files/sample.png
+      EOS
+      output
+    end
+  end
 
-	# Helper methods
-	helpers do
-		def file_not_found
-		  status 404
+  # Helper methods
+  helpers do
+    def file_not_found
+      status 404
       body "File not found :( \n"
-		end
+    end
 
-		def file_already_exists
+    def file_already_exists
       status 409
       body "File already exists\n"
-		end
+    end
 
-		def success (message)
-			status 200
-			body "File #{message} successfully! :D \n"
-		end
-	end
+    def success (message)
+      status 200
+      body "File #{message} successfully! :D \n"
+    end
+  end
 end
